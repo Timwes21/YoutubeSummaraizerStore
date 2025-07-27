d71abde20ca97a8e4d8be000d734d528ce750914
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-07-27 04:21:31 -0400

    made port int

--- server.py
+++ server.py
@@ -42,4 +42,4 @@ app.include_router(info_router, prefix="/get-info")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
